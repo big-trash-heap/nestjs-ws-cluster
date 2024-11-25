3d8082f53cfb9537212b5205430caa62c6c11777
givename <givename48@yandex.ru>
2024-11-25 14:36:37 +0500

    add: using redis-adapter

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './redis-io-adapter.class';
 
 console.log('I AM USE REDIS_URL - ' + process.env.REDIS_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useWebSocketAdapter(new IoAdapter(app));
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
 
   await app.listen(process.env.PORT ?? 3223);
 }
